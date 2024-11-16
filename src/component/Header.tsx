46a2db0a892aecd63e0f475ff28cfffba36a757d
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-11-16 07:42:51 +0100

    🔧 (package.json): add "sass" dependency to support SCSS stylesheets in the project 📝 (App.css): remove unused CSS file "App.css" from the project ✨ (wind.png): add wind.png image asset to the project ♻️ (Header.tsx): refactor Header component to use an image tag for the logo instead of text 🔧 (main.tsx): update import path to use the new SCSS file "index.scss" for styles 🔧 (App.scss, _variables.scss, Header.scss, index.scss): add SCSS files for styling components and define variables for consistent styling 🔧 (vite.config.ts): update Vite configuration to use SCSS preprocessor with source maps, set build options for output directory, chunk size warning limit, and CSS preprocessor options The changes were made to improve the project's styling capabilities by adding support for SCSS stylesheets, optimizing the project structure by removing unused files, adding necessary image assets, refactoring components for better design implementation, and updating Vite configuration for improved build and styling processes.

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,11 +1,9 @@
-import '../styles/Header.css';
-
 export const Header = () => {
   return (
     <header className="navbar">
       <div className="nav-container">
         <div className="nav-logo">
-          <span className="logo-text">Wind Data Planner</span>
+          <img src="/src/assets/wind.png" alt="logo" />
         </div>
       </div>
     </header>
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,24 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3000,
     host: true,
     strictPort: true,
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 600, // Adjust the limit if necessary
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Use the new API for scss preprocessing in Vite
+        // ref: https://vite.dev/config/shared-options#css-preprocessoroptions
+        api: 'modern-compiler', // or "modern"
+        sourceMap: true,
+      },
+    },
   },
 });
