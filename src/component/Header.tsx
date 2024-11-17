4808cc87ce40034ec787136b9b8bcf0da14efda5
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-11-17 12:33:11 +0100

    📝 (.github/PULL_REQUEST_TEMPLATE.md): Add a pull request template to provide guidelines for creating and reviewing pull requests ✨ (src/component/Header.tsx): import Logo from '../assets/wind.png'; and update image source to use imported Logo variable for better code organization and maintainability The pull request template was added to ensure consistency and provide guidelines for creating and reviewing pull requests. In the Header component, the image source was updated to use the imported Logo variable for better code organization and maintainability.

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,11 @@
+import Logo from '../assets/wind.png';
+
 export const Header = () => {
   return (
     <header className="navbar">
       <div className="nav-container">
         <div className="nav-logo">
-          <img src="/src/assets/wind.png" alt="logo" />
+          <img src={Logo} alt="logo" />
         </div>
       </div>
     </header>
